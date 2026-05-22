e540e4c79838b2be0273fd90251efbe67a5e067f
hannahflorence <hannahfbr15@gmail.com>
2026-05-22 12:56:05 +0800

    v1

---
+++ server/src/firebase.ts
@@ -0,0 +1,30 @@
+import admin from "firebase-admin";
+import { config } from "./config.js";
+
+let app: admin.app.App | null = null;
+
+export function getFirebaseApp(): admin.app.App {
+  if (app) return app;
+
+  if (config.useFirebaseEmulator) {
+    process.env.FIREBASE_DATABASE_EMULATOR_HOST =
+      config.firebaseDatabaseEmulatorHost;
+  }
+
+  if (!admin.apps.length) {
+    app = admin.initializeApp({
+      projectId: config.firebaseProjectId,
+      databaseURL: config.useFirebaseEmulator
+        ? `http://${config.firebaseDatabaseEmulatorHost}?ns=${config.firebaseProjectId}-default-rtdb`
+        : `https://${config.firebaseProjectId}-default-rtdb.firebaseio.com`,
+    });
+  } else {
+    app = admin.app();
+  }
+
+  return app;
+}
+
+export function getDb(): admin.database.Database {
+  return getFirebaseApp().database();
+}
